fe65db0226918a4b7abaa9947c2aefa2ecb53519
tedskiii <theodorekasyfillah06@gmail.com>
2023-09-11 21:00:42 +0700

    Menambahkan Gambar dikit

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
     price = models.CharField(max_length=255)
     description = models.TextField()
     amount = models.IntegerField()
--- main/views.py
+++ main/views.py
@@ -8,25 +8,22 @@ def show_main(request):
             'price': 'Rp.199.000',
             'description': 'T-Shirt dengan motif garis-garis dari bahan kualitas terbaik di Indonesia',
             'amount': 25,
-        },
-        {   
-            'name': 'Celana Jeans Slim Fit',
-            'price': 'Rp.299.000',
-            'description': 'Celana jeans dengan potongan slim fit dari bahan kualitas terbaik di Indonesia',
-            'amount': 15,
+            'image_url': 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/437241/item/goods_69_437241.jpg?width=750', 
         },
         {
             'name': 'Jaket Casual',
             'price': 'Rp.399.000',
             'description': 'Jaket casual dengan desain trendy dari bahan kualitas terbaik di Indonesia',
             'amount': 10,
+            'image_url': 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/459591/sub/goods_459591_sub14.jpg?width=750',
         },
         {
-            'name': 'Sepatu Sneakers',
-            'price': 'Rp.499.000',
-            'description': 'Sepatu sneakers dengan desain sporty dari bahan kualitas terbaik di Indonesia',
-            'amount': 20
-        }
+            'name': 'Celana Jeans Slim Fit',
+            'price': 'Rp.299.000',
+            'description': 'Celana jeans dengan potongan slim fit dari bahan kualitas terbaik di Indonesia',
+            'amount': 15,
+            'image_url': 'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/459688/sub/goods_459688_sub14.jpg?width=750', 
+        },
     ]
 
     context = {
@@ -34,3 +31,4 @@ def show_main(request):
     }
 
     return render(request, "main.html", context)
+
