40e3cb9943f424101ff9ba731533ad517dfe9ab2
tedskiii <theodorekasyfillah06@gmail.com>
2023-09-11 11:42:19 +0700

    Menambahkan UI Cards supaya terlihat lebih rapih

--- main/views.py
+++ main/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_main(request):
+    products = [
+        {
+            'name': 'T-Shirt Garis Lengan Pendek',
+            'price': 'Rp.199.000',
+            'description': 'T-Shirt dengan bahan katun kualitas terbaik dari Indonesia dengan motif garis-garis',
+            'amount': 25
+        },
+        {
+            'name': 'Celana Jeans Slim Fit',
+            'price': 'Rp.299.000',
+            'description': 'Celana jeans dengan potongan slim fit dengan bahan kualitas terbaik dari Indonesia yang nyaman digunakan sehari-hari',
+            'amount': 15
+        }
+    ]
+
     context = {
-        'name': 'T-Shirt Garis Lengan Pendek',
-        'price': 'Rp.199.000',
-        'description': 'T-Shirt dengan bahan katun kualitas terbaik dari Indonesia dengan motif garis garis',
-        'amount': 25
+        'products': products
     }
 
     return render(request, "main.html", context)
