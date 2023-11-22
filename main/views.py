a512e7b6ed9456e9103cb87d7bbe6e0d6dd82e0d
tedskiii <theodorekasyfillah06@gmail.com>
2023-11-22 09:22:01 +0700

    Push Tugas 9(uf)

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.core import serializers
@@ -99,3 +100,22 @@ def add_product_ajax(request):
         new_product.save()
 
         return HttpResponse(b"CREATED", status=201)
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
