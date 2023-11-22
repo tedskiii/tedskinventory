41ee1a27cabb6a47af097222d1ff0a9f89612547
tedskiii <theodorekasyfillah06@gmail.com>
2023-11-22 15:29:05 +0700

    Fix Deployment Error

--- main/views.py
+++ main/views.py
@@ -52,6 +52,7 @@ def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+@csrf_exempt
 def register(request):
     form = UserCreationForm()
     if request.method == "POST":
@@ -63,6 +64,7 @@ def register(request):
     context = {'form':form}
     return render(request, 'register.html', context)
 
+@csrf_exempt
 def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
