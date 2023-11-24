ba9d79c1363c4c02f1827ca03b3de94780b4f004
tedskiii <theodorekasyfillah06@gmail.com>
2023-11-24 14:42:50 +0700

    Fix Deployment Error

--- tedskinventory/settings.py
+++ tedskinventory/settings.py
@@ -67,7 +67,7 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
-CSRF_TRUSTED_ORIGINS = "https://theodoore-kasyfillah-tugas.pbp.cs.ui.ac.id/"
+CSRF_TRUSTED_ORIGINS = ["https://theodoore-kasyfillah-tugas.pbp.cs.ui.ac.id/"]
 
 TEMPLATES = [
     {
